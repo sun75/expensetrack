b296645eac3b3c294a94c46c8088ff2090ef65be
finalfrontier <1123525416@qq.com>
2025-01-30 00:48:47 +0100

    add configuration of railway deploy

--- src/main/java/com/expense/expensetrack/ExpensetrackApplication.java
+++ src/main/java/com/expense/expensetrack/ExpensetrackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com/expense/expensetrack/entity")
+//@EntityScan(basePackages = "com/expense/expensetrack/entity")
 public class ExpensetrackApplication {
 
 	public static void main(String[] args) {
